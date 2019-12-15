312b43bd6a88560ac433f7bbd32c095ba894e993
lum <lum@netcompany.com>
2019-12-16 00:11:37 +0100

    Added FlatList with items

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, FlatList, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, Image} from 'react-native';
 
 export default class App extends React.Component {
 
@@ -10,7 +10,7 @@ export default class App extends React.Component {
 
   fetchPhotos = () => {
     this.setState({ isLoading: true });
-    fetch("https://jsonplaceholder.typicode.com/photos?_start=1&_limit=200")
+    fetch("https://jsonplaceholder.typicode.com/photos?_start=1&_limit=40")
       .then(data => data.json())
       .then(photos => {
         this.setState({ 
@@ -45,10 +45,16 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <ScrollView>
-          {photos.map(photo => <Text>{photo.title}</Text>)}
-          <Button title="Reload" onPress={this.fetchPhotos}/>
-        </ScrollView>
+        <FlatList
+          data={photos}
+          renderItem={({item}) => 
+          <View style={{flex:1, flexDirection: 'row', marginRight: 5}}>
+            <Text style={styles.textView}>{item.title}</Text>
+            <Image source = {{ uri: item.thumbnailUrl }} style={styles.imageView} />
+          </View>}
+          keyExtractor={photo => photo.id}
+        />
+        <Button title="Reload" onPress={this.fetchPhotos}/>
       </View>
     );
   } 
@@ -59,6 +65,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 5
   },
+  imageView: {
+    width: '50%',
+    height: 100 ,
+    margin: 7,
+    borderRadius : 7
+  },
+  textView: {
+    width:'50%', 
+    textAlignVertical:'center',
+    padding:10,
+    color: '#000'
+  }
 });
